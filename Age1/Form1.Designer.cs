dcc17f874d24ebc568b542c55eb8b7ddae3db06b
Maya Kali <Mayasophiakali@outlook.com>
2021-06-15 14:30:41 -0700

    Add project files.

---
+++ Age1/Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿
+namespace Age1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.Result1 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Year2 = new System.Windows.Forms.TextBox();
+            this.Day2 = new System.Windows.Forms.TextBox();
+            this.Month2 = new System.Windows.Forms.TextBox();
+            this.Year1 = new System.Windows.Forms.TextBox();
+            this.Day1 = new System.Windows.Forms.TextBox();
+            this.Month1 = new System.Windows.Forms.TextBox();
+            this.Clear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(325, 295);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(248, 48);
+            this.button1.TabIndex = 34;
+            this.button1.Text = "Calculate age";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Result1
+            // 
+            this.Result1.Location = new System.Drawing.Point(230, 369);
+            this.Result1.Multiline = true;
+            this.Result1.Name = "Result1";
+            this.Result1.Size = new System.Drawing.Size(471, 41);
+            this.Result1.TabIndex = 33;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(663, 175);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 17);
+            this.label5.TabIndex = 32;
+            this.label5.Text = "Year";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(442, 175);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(33, 17);
+            this.label4.TabIndex = 31;
+            this.label4.Text = "Day";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(228, 175);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(47, 17);
+            this.label9.TabIndex = 30;
+            this.label9.Text = "Month";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(48, 227);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(60, 17);
+            this.label8.TabIndex = 29;
+            this.label8.Text = "Birthday";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(48, 84);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(89, 17);
+            this.label7.TabIndex = 28;
+            this.label7.Text = "Todays Date";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(663, 40);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 17);
+            this.label3.TabIndex = 27;
+            this.label3.Text = "Year";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(442, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 17);
+            this.label2.TabIndex = 26;
+            this.label2.Text = "Day";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(228, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 17);
+            this.label1.TabIndex = 25;
+            this.label1.Text = "Month";
+            // 
+            // Year2
+            // 
+            this.Year2.Location = new System.Drawing.Point(598, 206);
+            this.Year2.Multiline = true;
+            this.Year2.Name = "Year2";
+            this.Year2.Size = new System.Drawing.Size(155, 38);
+            this.Year2.TabIndex = 24;
+            // 
+            // Day2
+            // 
+            this.Day2.Location = new System.Drawing.Point(391, 206);
+            this.Day2.Multiline = true;
+            this.Day2.Name = "Day2";
+            this.Day2.Size = new System.Drawing.Size(155, 38);
+            this.Day2.TabIndex = 23;
+            // 
+            // Month2
+            // 
+            this.Month2.Location = new System.Drawing.Point(182, 206);
+            this.Month2.Multiline = true;
+            this.Month2.Name = "Month2";
+            this.Month2.Size = new System.Drawing.Size(155, 38);
+            this.Month2.TabIndex = 22;
+            // 
+            // Year1
+            // 
+            this.Year1.Location = new System.Drawing.Point(598, 72);
+            this.Year1.Multiline = true;
+            this.Year1.Name = "Year1";
+            this.Year1.Size = new System.Drawing.Size(155, 38);
+            this.Year1.TabIndex = 21;
+            // 
+            // Day1
+            // 
+            this.Day1.Location = new System.Drawing.Point(391, 72);
+            this.Day1.Multiline = true;
+            this.Day1.Name = "Day1";
+            this.Day1.Size = new System.Drawing.Size(155, 38);
+            this.Day1.TabIndex = 20;
+            // 
+            // Month1
+            // 
+            this.Month1.Location = new System.Drawing.Point(182, 72);
+            this.Month1.Multiline = true;
+            this.Month1.Name = "Month1";
+            this.Month1.Size = new System.Drawing.Size(155, 38);
+            this.Month1.TabIndex = 19;
+            // 
+            // Clear
+            // 
+            this.Clear.Location = new System.Drawing.Point(325, 441);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(248, 33);
+            this.Clear.TabIndex = 35;
+            this.Clear.Text = "Clear";
+            this.Clear.UseVisualStyleBackColor = true;
+            this.Clear.Click += new System.EventHandler(this.Clear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(821, 516);
+            this.Controls.Add(this.Clear);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Result1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Year2);
+            this.Controls.Add(this.Day2);
+            this.Controls.Add(this.Month2);
+            this.Controls.Add(this.Year1);
+            this.Controls.Add(this.Day1);
+            this.Controls.Add(this.Month1);
+            this.Name = "Form1";
+            this.Text = "Age caculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox Result1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Year2;
+        private System.Windows.Forms.TextBox Day2;
+        private System.Windows.Forms.TextBox Month2;
+        private System.Windows.Forms.TextBox Year1;
+        private System.Windows.Forms.TextBox Day1;
+        private System.Windows.Forms.TextBox Month1;
+        private System.Windows.Forms.Button Clear;
+    }
+}
+
---
+++ Age1/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Age1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Month1.Text = DateTime.Today.Month.ToString();
+            Day1.Text = DateTime.Today.Day.ToString();
+            Year1.Text = DateTime.Today.Year.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Creating Variables to 
+            int By, Bm, Bd, Cy, Cm, Cd;
+            int years, months, days;
+            Bm = Convert.ToInt32(Month2.Text);
+            By = Convert.ToInt32(Year2.Text);
+            Bd = Convert.ToInt32(Day2.Text);
+
+            Cm = Convert.ToInt32(Month1.Text);
+            Cy = Convert.ToInt32(Year1.Text);
+            Cd = Convert.ToInt32(Day1.Text);
+         
+
+
+
+            if (Cy <0 || Cy >2121)
+            {
+                MessageBox.Show("wrong Date, Try again");
+                return;
+
+            }
+            if (By < 0 || By > 2021) 
+            {
+                MessageBox.Show("wrong Date, Try again");
+                return;
+            }
+            if (Cm < 0 || Cm > 12 || Bm < 0 || Bm > 12)
+            {
+                MessageBox.Show("wrong Date, Try again");
+                return;
+            }
+            if (Cd < 0 || Cd > 31 || Bd < 0 || Bd > 31)
+            {
+                MessageBox.Show("wrong Date, Try again");
+                return;
+            }
+            years = Cy - By;
+            months = Cm - Bm;
+            days = Cd - Bd;
+
+            if ( days < 0)
+            {
+                months = months - 1;
+                days = days + 30;
+
+            }
+            if (months < 0)
+            {
+                years = years - 1;
+                months = months + 12;
+
+
+            }
+
+            Result1.Text = " You are " + years.ToString() + " years and " + months.ToString() + " months and" + days.ToString() + " days";
+        }
+
+        private void Clear_Click(object sender, EventArgs e)
+        {
+            Month2.Clear();
+            Year2.Clear();
+            Day2.Clear();
+            Result1.Clear();
+        }
+    }
+}
